ca3eae94306eb061b6ae63c1bd07f642e41cb39c
Sujal Pahwa <142010621+sujalpahwa30@users.noreply.github.com>
2024-07-17 00:18:33 +0530

    Added paddle collision logic

--- script.js
+++ script.js
@@ -24,14 +24,23 @@ document.addEventListener("DOMContentLoaded" , () => {
         // if(ballX > 700-20 || ballX <= 0) dx *= -1;
         // if(ballY > 400-20 || ballY <= 0) dy *= -1;
 
+        if(ballX < paddle.offsetLeft + paddle.offsetWidth &&
+            ballY > paddle.offsetTop && 
+            ballY - ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight
+        ) {
+            dx *= -1;
+        }
+
         if(ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1; // change x-direction
         if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1; // change y-direction
+
 
     } , 1);
 
     let paddleY = 0;
-    let dPy = 5; // displacement for paddle in y-direction 
+    let dPy = 10; // displacement for paddle in y-direction 
     document.addEventListener("keydown" , (event) => {
+        event.preventDefault(); // prevents the execution of the default event behaviour 
         if(event.keyCode == 38 && paddleY > 0) {
             // up arrow
             paddleY += (-1)*dPy;
